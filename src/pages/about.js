edb82b8bb031191aa2cff622bb6df3ae3d06d134
Tony Porter <tonyporter89@gmail.com>
2019-01-14 21:08:05 +0000

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is some text</p>
+    </div>
+  )
+}
+
+export default AboutPage;
