3478032eb3bac0c8e8fc2199700cbc10c85ff784
Tony Porter <tonyporter89@gmail.com>
2019-01-25 21:24:11 +0000

    updated dependencies for next stage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import '../styles/main.scss'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
