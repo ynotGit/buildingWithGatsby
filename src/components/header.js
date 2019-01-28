e1916a0c5c00dfa26f568abb6172fff5d8783fdd
Tony Porter <tonyporter89@gmail.com>
2019-01-28 14:36:28 +0000

    to flex or not to flex, that is the question

--- src/components/header.js
+++ src/components/header.js
@@ -9,13 +9,26 @@ const HeaderWrapper = styled.div `
 
 const HeaderContainer = styled.div `
   margin: 0 auto;
-  max-width: 960px;
-  padding: 1.45rem 1.0875rem;
+  padding: 1rem 0;
+  display: flex;
+  align-items: center;
+  text-align: center;
 `
 
 const StyledLink = styled(Link)`
-    color: white;
-    text-decoration: none;
+  flex: 1;
+  color: white;
+  text-decoration: none;
+`
+
+const Test1 = styled.div `
+  flex: 1;
+  color: red;
+`
+
+const Test2 = styled.div `
+  flex: 1;
+  color: blue;
 `
 
 const Header = () => (
@@ -24,6 +37,8 @@ const Header = () => (
       <StyledLink to="/">
         <h1>Hello</h1>    
       </StyledLink>
+      <Test1>Test 1</Test1>
+      <Test2>Test 2</Test2>
     </HeaderContainer>
   </HeaderWrapper>
 )
