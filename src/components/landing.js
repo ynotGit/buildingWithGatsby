043235bfdac4b55823875789c43818b9038d4a23
Tony Porter <tonyporter89@gmail.com>
2019-02-06 00:36:13 +0000

    importanted landing image

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import LandingBackground from '../images/landingBackground.png'
+
 const LandingWrapper = styled.div `
-    background: whitesmoke;
+    background: url(${LandingBackground});
+    width: 100%;
+    height: 400px;
+    background-size: cover;
+    background-position: center;
 `
 
 const LandingContent = styled.div `
-    
+
 `
 
 const Landing = () => (
     <LandingWrapper>
         <LandingContent>
-            test
+        
         </LandingContent>
     </LandingWrapper>
 )
