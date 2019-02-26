32fc123b74ea34ed7921d3616f7db51b029d5ed2
Tony Porter <tonyporter89@gmail.com>
2019-02-26 00:35:18 +0000

    updated config file for markdown images

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,16 +18,11 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-remark-images`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        maxWidth: 590,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      }, 
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
@@ -39,6 +34,19 @@ module.exports = {
         name: "markdown-pages",
       },
     },
-    `gatsby-transformer-remark`,
+    {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 800,
+              }
+            }
+        ]
+      }
+    }
   ],
 }
+
