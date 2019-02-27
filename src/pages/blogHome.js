77520bb7a3905c50978b8f5ab3701500d9c03b4c
Tony Porter <tonyporter89@gmail.com>
2019-02-27 01:05:06 +0000

    began structure blog home layout

--- src/pages/blogHome.js
+++ src/pages/blogHome.js
@@ -2,24 +2,45 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { graphql } from 'gatsby'
 import SEO from '../components/seo'
-import Img from 'gatsby-image'
+import CoverImage from 'gatsby-image'
+import styled from 'styled-components'
       
 import Layout from '../components/layout'
 import '../styles/main.scss'
+
+const BlogHomeWrapper = styled.section `
+  width: 100%;
+`
+
+const BlogHomeContainer = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const coverImage = styled.img `
+  flex: 1;
+  
+`
+
       
 const BlogHome = ({data}) => (
     <Layout>
         <SEO title="Blog Home" keywords={['blog']} />
-        {data.allMarkdownRemark.edges.map(post => (
-        <Link 
-          key={post.node.id}
-          to={post.node.frontmatter.path} 
-        >
-          {post.node.frontmatter.date}
-          <Img fluid={post.node.frontmatter.coverImage.childImageSharp.fluid} />
-          {post.node.frontmatter.title}
-        </Link>
-      ))}  
+        <BlogHomeWrapper>
+          <BlogHomeContainer>
+            {data.allMarkdownRemark.edges.map(post => (
+            <Link 
+              key={post.node.id}
+              to={post.node.frontmatter.path} 
+            >
+              {post.node.frontmatter.date}
+              <CoverImage fluid={post.node.frontmatter.coverImage.childImageSharp.fluid} />
+              {post.node.frontmatter.title}
+            </Link>
+          ))}  
+        </BlogHomeContainer>
+      </BlogHomeWrapper>
     </Layout>
 )
       
