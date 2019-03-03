d0247af45f583e69ad53e7c903d8e7f0ce089131
Tony Porter <tonyporter89@gmail.com>
2019-03-03 10:52:52 +0000

    conv contact form to styled components & added more styling,  added success.js page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaEnvelopeOpenText } from 'react-icons/fa'
+import { FaEnvelopeOpenText, FaRegAddressCard, FaRegEnvelope, FaMobileAlt, FaBuilding, FaCommentAlt } from 'react-icons/fa'
 
 const ContactWrapper = styled.section `
     text-align: center;
@@ -8,6 +8,36 @@ const ContactWrapper = styled.section `
 
 const ContactContainer = styled.div `
 
+`
+
+const Form = styled.form `
+    padding: 30px;
+`
+
+const ContactRow = styled.div `
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+`
+
+const ContactColumn = styled.div `
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+`
+
+const FormInput = styled.input `
+    margin: 10px;
+`
+
+const FormLabel = styled.label `
+    margin: 10px 0;
+`
+
+const FromTextArea = styled.textarea `
+    margin: 0 10px;
 `
 
 // - top envelope icons
@@ -19,34 +49,49 @@ const Contact = () => (
     <ContactWrapper>
         <ContactContainer>
             <h1>Contact</h1>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="bot-field" />
+            <Form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+                <FormInput type="hidden" name="bot-field" />
                 <FaEnvelopeOpenText />
-                <div className="field half first">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" />
-                </div>
-                <div className="field half">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
-                </div>
-                <div className="field half">
-                    <label htmlFor="number">Number</label>
-                    <input type="number" name="number" id="number" />
-                </div>
-                <div className="field half">
-                    <label htmlFor="company">Company</label>
-                    <input type="text" name="company" id="company" />
-                </div>
-                <div className="field">
-                    <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message" rows="6"></textarea>
-                </div>
-                <ul className="actions">
-                    <li><input type="submit" value="Send" className="special" /></li>
-                    <li><input type="reset" value="Clear" /></li>
-                </ul>
-            </form>
+                <ContactRow>
+                    <ContactColumn>
+                        <FaRegAddressCard />
+                        <FormLabel htmlFor="name">Name</FormLabel>
+                        <FormInput type="text" name="name" id="name" />
+                    </ContactColumn>
+                    <ContactColumn>
+                        <FaRegEnvelope />
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <FormInput type="email" name="email" id="email" />
+                    </ContactColumn>
+                </ContactRow>
+                <ContactRow>
+                    <ContactColumn>
+                        <FaMobileAlt />
+                        <FormLabel htmlFor="number">Number</FormLabel>
+                        <FormInput type="number" name="number" id="number" />
+                    </ContactColumn>
+                    <ContactColumn>
+                        <FaBuilding />
+                        <FormLabel htmlFor="company">Company</FormLabel>
+                        <FormInput type="text" name="company" id="company" />
+                    </ContactColumn>
+                </ContactRow>
+                <ContactRow>
+                    <ContactColumn>
+                        <FaCommentAlt />
+                        <FormLabel htmlFor="message">Message</FormLabel>
+                        <FromTextArea name="message" id="message" rows="6"></FromTextArea>
+                    </ContactColumn>
+                </ContactRow>
+                <ContactRow>
+                    <ContactColumn>
+                        <FormInput type="submit" value="Send" className="special" />
+                    </ContactColumn>
+                    <ContactColumn>    
+                        <FormInput type="reset" value="Clear" />
+                    </ContactColumn>
+                </ContactRow>
+            </Form>
         </ContactContainer>
     </ContactWrapper>
 )
---
+++ src/pages/success.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+import TestPic from '../images/logo.png'
+
+const Success = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Success Page</title>
+            <meta name="description" content="Success Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Success/Thank You Page</h1>
+                    </header>
+                    <span className="image main"><img src={TestPic} alt="Logo" /></span>
+                    <p>Thank you for contacting us!</p>
+                </div>
+            </section>
+        </div>
+    </Layout>
+)
+
+export default Success
