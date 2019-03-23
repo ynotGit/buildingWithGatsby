75763e38399cb426e87754cd79707e402be0c73c
Tony Porter <tonyporter89@gmail.com>
2019-03-23 10:47:18 +0000

    added react-media npm for conditional rendering of components based on device width

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog Site`,
+    description: `A full site blog build using Gatsby`,
+    author: `@ynotinnovate`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
