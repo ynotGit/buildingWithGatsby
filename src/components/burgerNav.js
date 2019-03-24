a18926714c47f91911e98e99ab9b4ab2a398f1b1
Tony Porter <tonyporter89@gmail.com>
2019-03-24 13:50:55 +0000

    finished BurgerMenu styling;

--- src/components/burgerNav.js
+++ src/components/burgerNav.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 import { elastic as BurgerMenu } from 'react-burger-menu'
+import {
+  FaHome,
+  FaDiceD20,
+  FaDraftingCompass,
+  FaFeatherAlt,
+  FaPaperPlane,
+} from 'react-icons/fa'
 
 const styles = {
   bmBurgerButton: {
@@ -30,6 +38,7 @@ const styles = {
     background: '#373a47',
     padding: '2.5em 1.5em 0',
     fontSize: '1.15em',
+    textAlign: 'center',
     display: 'flex',
     flexDirection: 'column',
   },
@@ -39,36 +48,55 @@ const styles = {
   bmItemList: {
     color: '#b8b7ad',
     padding: '0.8em',
-    textDecoration: 'none',
   },
   bmItem: {
-    textAlign: 'center',
     display: 'block',
     color: 'white',
     margin: '50% 0',
+    textDecoration: 'none',
+    letterSpacing: '5px',
   },
   bmOverlay: {
     background: 'rgba(0, 0, 0, 0.3)',
   },
 }
 
+const Icon = styled.span`
+  margin-right: 10px;
+`
+
 const isMenuOpen = state => state.isOpen
 
 const BurgerNav = () => (
   <BurgerMenu right onStateChange={isMenuOpen} styles={styles}>
     <a id="home" className="menu-item" href="/">
+      <Icon>
+        <FaHome />
+      </Icon>
       Home
     </a>
     <a id="about" className="menu-item" href="/">
+      <Icon>
+        <FaDiceD20 />
+      </Icon>
       About
     </a>
     <a id="services" className="menu-item" href="/">
+      <Icon>
+        <FaDraftingCompass />
+      </Icon>
       Services
     </a>
     <a id="projects" className="menu-item" href="/">
+      <Icon>
+        <FaFeatherAlt />
+      </Icon>
       Projects
     </a>
     <a id="contact" className="menu-item" href="/">
+      <Icon>
+        <FaPaperPlane />
+      </Icon>
       Contact
     </a>
   </BurgerMenu>
